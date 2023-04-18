44dc9c1c5283c0a3886bfa26765a52cdc99b0582
SimyV <s.lobascher@gmail.com>
2023-04-18 18:21:47 +1000

    new

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ const init = function () {
 
 generateActivity.addEventListener("click", function () {
   newImage = Math.trunc(Math.random() * currentActivity.length) + 1;
-  console.log(newImage);
+  // console.log(newImage);
 
   imageChange.classList.remove("hidden");
   imageChange.src = `/images/image-${newImage}.png`;
